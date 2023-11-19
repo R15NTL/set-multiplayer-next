1fb1ed2c7a65986b124a807416ff8ed9a681f1b5
R15NTL <120052967+R15NTL@users.noreply.github.com>
2023-11-19 15:44:40 +0000

    feat: min-height 100dvh

--- src/layouts/mainLayout/MainLayout.tsx
+++ src/layouts/mainLayout/MainLayout.tsx
@@ -13,6 +13,7 @@ export default function MainLayout({ children, className }: MainLayoutProps) {
   return (
     <div
       className={cn(`min-h-screen flex flex-col pt-header-height`, className)}
+      style={{ minHeight: "100dvh" }}
     >
       <Header />
       <main className="flex-grow flex px-page-x-padding py-5">{children}</main>
