7340536c668de0d4ecda80f5bff07d703a423b61
R15NTL <120052967+R15NTL@users.noreply.github.com>
2024-01-15 20:30:18 +0000

    Homepage animation removed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,35 +6,8 @@ import MainLayout from "../layouts/mainLayout/MainLayout";
 // Components
 import MenuButton from "@/features/home/MenuButton";
 import IconCircle from "@/features/home/IconCircle";
-// Animate
-import { motion } from "framer-motion";
 // Paths
 import { paths } from "@/routes/paths";
-// Auth
-import EmailVerifiedGuard from "@/features/auth/EmailVerifiedGuard/EmailVerifiedGuard";
-
-// Stagger effect
-const buttonVariants = {
-  hidden: { opacity: 0, y: 100 },
-  show: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      duration: 0.5,
-      ease: "easeInOut",
-    },
-  },
-};
-const containerVariants = {
-  hidden: { opacity: 0, y: 100 },
-  show: {
-    y: 0,
-    opacity: 1,
-    transition: {
-      staggerChildren: 0.2,
-    },
-  },
-};
 
 Index.getLayout = (page: React.ReactNode) => (
   <MainLayout className="px-page-x-padding">{page}</MainLayout>
@@ -46,19 +19,14 @@ export default function Index() {
       <Head>
         <title>Set Multiplayer</title>
       </Head>
-      <motion.div
-        className="py-page-y-padding m-auto flex-1 h-full flex items-center flex-grow"
-        variants={containerVariants}
-        initial="hidden"
-        animate="show"
-      >
-        <motion.div className="m-auto flex gap-5 flex-col w-full max-w-lg">
-          <motion.div className="flex flex-col" variants={buttonVariants}>
+      <div className="py-page-y-padding m-auto flex-1 h-full flex items-center flex-grow">
+        <div className="m-auto flex gap-5 flex-col w-full max-w-lg">
+          <div className="flex flex-col">
             <MenuButton title="Multiplayer" href={paths.multiplayer.lobby.root}>
               <IconCircle icon="solar:users-group-two-rounded-bold" />
             </MenuButton>
-          </motion.div>
-          <motion.div className="flex flex-col" variants={buttonVariants}>
+          </div>
+          <div className="flex flex-col">
             <MenuButton
               className="single-player"
               href={paths.singlePlayer.root}
@@ -66,8 +34,8 @@ export default function Index() {
             >
               <IconCircle icon="solar:user-bold" />
             </MenuButton>
-          </motion.div>
-          <motion.div className="flex flex-col" variants={buttonVariants}>
+          </div>
+          <div className="flex flex-col">
             <MenuButton
               href={paths.howToPlay.root}
               className="settings"
@@ -75,9 +43,9 @@ export default function Index() {
             >
               <IconCircle icon="solar:book-bookmark-bold" />
             </MenuButton>
-          </motion.div>
-        </motion.div>
-      </motion.div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
